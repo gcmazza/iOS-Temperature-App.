7dcbe8c481887f6a8385ca72d9d3fd110b69191b
gcmazza <84994030+gcmazza@users.noreply.github.com>
2025-12-18 11:17:35 -0500

    Initial commit: Add basic setup files for iOS development.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# iOS-Temperature-App
-Dynamically Changes screen graphics based on temperature readings
+# iOS Temperature App
+
+This is an iOS application that displays temperature data.
+
+## Features
+- Display temperature
+
+## Getting Started
+To begin working, clone this repository and open it in Xcode. Follow iOS app deployment guidelines.
---
+++ TemperatureApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TemperatureApp
+//
+//  Created by gcmazza on 2025-12-18.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
